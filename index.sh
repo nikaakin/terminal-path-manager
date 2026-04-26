ee10530cf7bc64927d7be7f1879f27e92ee82993
Nika Tsutskiridze <nikacuckiridze47@gmail.com>
2026-04-26 15:17:00 +0400

    fix index.sh to correctly use HOME variable for specific search paths

--- index.sh
+++ index.sh
@@ -4,12 +4,12 @@ path=$(node $dir/index.js $@)
 # s or search flag
 
 # some more specific paths that have some directories I want to search through
-specific_paths=("~/Desktop ~/Desktop/practice")
+specific_paths="$HOME/Desktop $HOME/Desktop/practice"
 
 if [[ $1 = "s" || $1 = "search" ]]
     # if you only want to search through the paths you have added with cli remove `$(find ~/Desktop -mindepth 1 -maxdepth 2 -type d)` this
     # or adjust find script for your needs
-    then path=$(printf "$path\n$(find ${specific_paths[@]}  -mindepth 1 -maxdepth 2 -type d -not -name '.*')" | awk 'NF && !seen[$0]++' | fzf)
+    then path=$(printf "$path\n$(find $specific_paths  -mindepth 1 -maxdepth 2 -type d -not -name '.*')" | awk 'NF && !seen[$0]++' | fzf)
 fi
 
 
