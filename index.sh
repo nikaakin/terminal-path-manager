5f864c1af1c1e0a5d92c4e51bc27e2e1a00f9d74
Nika Tsutskiridze <nikacuckiridze47@gmail.com>
2026-04-26 15:13:55 +0400

    enhance index.sh to include specific search paths and eliminate duplicates in search results

--- index.sh
+++ index.sh
@@ -3,10 +3,13 @@ dir=$(dirname "$(readlink -f "$0")")
 path=$(node $dir/index.js $@)
 # s or search flag
 
+# some more specific paths that have some directories I want to search through
+specific_paths=("~/Desktop ~/Desktop/practice")
+
 if [[ $1 = "s" || $1 = "search" ]]
     # if you only want to search through the paths you have added with cli remove `$(find ~/Desktop -mindepth 1 -maxdepth 2 -type d)` this
     # or adjust find script for your needs
-    then path=$(printf "$path\n$(find  ~/Desktop -mindepth 1 -maxdepth 2 -type d -not -name '.*')" | fzf)
+    then path=$(printf "$path\n$(find ${specific_paths[@]}  -mindepth 1 -maxdepth 2 -type d -not -name '.*')" | awk 'NF && !seen[$0]++' | fzf)
 fi
 
 
